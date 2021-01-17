2b81ac34e2316bf30aa79d02030059bb0e4dbfc9
Dhiraj Singh Dafouti <70090770+codingrock-star@users.noreply.github.com>
2021-01-17 22:08:04 +0530

    The DataStructures Folder Creation.

--- LinkedList/src/LinkedListOperation.java
+++ LinkedList/src/LinkedListOperation.java
@@ -48,56 +48,6 @@ public class LinkedListOperation {
     }
 
 
-
-    static Node head;
-
-    static class Node {
-        int data;
-        Node next;
-
-        Node(int data) {
-            this.data = data;
-            next = null;
-        }
-
-        void push() {
-
-        }
-
-        void insertAfter() {
-
-        }
-
-        void insertFront() {
-
-        }
-
-        void insertAtLocation(int position) {
-
-        }
-
-        void deleteAfter(int position) {
-
-        }
-
-        void deleteSpecficElement(int data) {
-
-        }
-
-        void deleteFirstElement() {
-
-        }
-
-        void deleteLastElement() {
-
-        }
-
-        int sizeOfList() {
-            return 0;
-        }
-    }
-
-
     public static void main(String[] args) {
         System.out.println("The Linked List Opertaion.....");
     }
