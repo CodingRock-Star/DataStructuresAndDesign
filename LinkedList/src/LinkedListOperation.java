b26f07436ace1328acf6b4b852e23917a31bf1da
Dhiraj Singh Dafouti <70090770+codingrock-star@users.noreply.github.com>
2021-03-29 15:20:13 +0530

    The DataStructures Folder Creation.

--- LinkedList/src/LinkedListOperation.java
+++ LinkedList/src/LinkedListOperation.java
@@ -10,44 +10,91 @@ public class LinkedListOperation {
             this.data = data;
             next = null;
         }
-        void push() {
+    }
 
+    void pushAtFront(int data) {
+
+    }
+
+    void pushAtTheEnd(int data) {
+
+    }
+
+    void push(int data) {
+        Node new_node;
+        System.out.println("The Data is pushed" + data);
+        if (head == null) {
+            new_node = new Node(data);
+            head = new_node;
+        }else {
+            Node n1=head;
+            if(n1.next!=null) {
+                n1=n1.next;
+            }
+            new_node = new Node(data);
+            n1.next = new_node;
+            new_node.next = null;
         }
 
-        void insertAfter() {
 
+    }
+
+    void printTheList() {
+        Node n = head;
+        while (n != null) {
+            System.out.println(n.data);
+            n = n.next;
         }
 
-        void insertFront() {
+    }
 
+
+    void insertAtLocation(int position) {
+
+    }
+
+    void deleteAfter(int position) {
+
+    }
+
+    void deleteSpecficElement(int data) {
+
+    }
+
+    void deleteFirstElement() {
+
+    }
+
+    void deleteLastElement() {
+
+    }
+
+    int sizeOfList() {
+        Node n = head;
+        int size = 0;
+        if (n != null) {
+            size++;
+            n = n.next;
         }
-
-        void insertAtLocation(int position) {
-
-        }
-
-        void deleteAfter(int position) {
-
-        }
-        void deleteSpecficElement(int data) {
-
-        }
-
-        void deleteFirstElement() {
-
-        }
-
-        void deleteLastElement() {
-
-        }
-
-        int sizeOfList() {
-            return 0;
-        }
+        return size;
     }
 
 
     public static void main(String[] args) {
         System.out.println("The Linked List Opertaion.....");
+
+        //pushing the data onto the list
+        LinkedListOperation listOperation = new LinkedListOperation();
+        listOperation.push(2);
+        listOperation.push(3);
+        listOperation.push(4);
+
+        //printing the List
+        listOperation.printTheList();
+
+        //The Size of list
+        listOperation.sizeOfList();
+
+
     }
 }
