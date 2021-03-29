ed343fe1d870d73630306bf5b9a23a8a2de034c1
Dhiraj Singh Dafouti <70090770+codingrock-star@users.noreply.github.com>
2021-03-29 15:25:08 +0530

    The DataStructures Folder Creation.

--- LinkedList/src/LinkedListOperation.java
+++ LinkedList/src/LinkedListOperation.java
@@ -93,7 +93,7 @@ public class LinkedListOperation {
         listOperation.printTheList();
 
         //The Size of list
-        listOperation.sizeOfList();
+        System.out.println(listOperation.sizeOfList());
 
 
     }
